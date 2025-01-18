9bc6bf70826e3445b3eec6fb0e3e05e089efe6f5
Nathan Cochran <58408117+nacochran@users.noreply.github.com>
2025-01-18 16:42:25 -0600

    Created InputManager, KeyManager, and MouseManager

---
+++ client/src/EventObjects/InputManager.js
@@ -0,0 +1,13 @@
+// client/src/EventObjects/InputManager.js
+
+export default class InputManager {
+  constructor() {
+    this.events = [];
+    this.current = {};
+  }
+
+  // creates event listeners for events
+  handleEvents() { }
+
+  // TODO: track events...
+}
