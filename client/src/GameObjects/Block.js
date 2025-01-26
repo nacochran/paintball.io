af2420cecc2f8f52a5c44756f9cef705c3ca713a
Nathan Cochran <58408117+nacochran@users.noreply.github.com>
2025-01-25 23:21:34 -0600

    Got Collisions Working

--- client/src/GameObjects/Block.js
+++ client/src/GameObjects/Block.js
@@ -1,7 +1,24 @@
 import StaticEntity from "./StaticEntity.js";
+import { Shape } from "../utils/ShapeHelper.js";
+import { Vec3 } from "../utils/vector.js";
 
 export default class Block extends StaticEntity {
   constructor(config) {
     super(config);
+
+    this.size = { width: 50, height: 50, depth: 50 };
+
+    this.shape = new Shape({
+      type: 'cube',
+      size: this.size,
+      position: this.position,
+      color: 0xaaaaaa
+    });
+    this.shape.attach(this);
+  }
+
+  update() {
+    // update shape's position, size, and orientation
+    this.shape.update();
   }
 }
