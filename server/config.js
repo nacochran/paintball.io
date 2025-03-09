59983b62b340442f542f5c4a6a2a560e6c81fa9a
Nathan Cochran <58408117+nacochran@users.noreply.github.com>
2025-03-08 19:21:14 -0600

    Switched to PostgreSQL

--- server/config.js
+++ server/config.js
@@ -1,16 +1,35 @@
 import dotenv from "dotenv";
+import { URL } from "url"; // For parsing the connection string
 
 dotenv.config();
+
+let databaseUrl;
+
+// heroku connection string
+// VS
+// local .env file
+if (process.env.DATABASE_URL) {
+  databaseUrl = new URL(process.env.DATABASE_URL);
+} else {
+  databaseUrl = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: 5432
+  };
+}
 
 const config = {
   app: {
     port: process.env.PORT || 5000,
   },
   db: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    host: databaseUrl.hostname,
+    user: databaseUrl.username,
+    password: databaseUrl.password,
+    database: (process.env.DATABASE_URL) ? databaseUrl.pathname.split('/')[1] : databaseUrl.database,
+    port: databaseUrl.port || 5432,
   },
   session: {
     secret: process.env.SESSION_SECRET,
--- server/main.sql
+++ server/main.sql
@@ -1,21 +1,21 @@
 DROP DATABASE IF EXISTS paintball_db;
 CREATE DATABASE paintball_db;
 
-USE paintball_db;
+\c paintball_db;
 
 -- Users who are registered but not verified yet
 CREATE TABLE unverified_users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(200) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     verification_code CHAR(6) NOT NULL,
-    token_expires_at DATETIME NOT NULL,  -- Token expires in 5 minutes
-    created_at DATETIME NOT NULL DEFAULT NOW()  -- User expires in 7 days
+    token_expires_at TIMESTAMPTZ NOT NULL,  -- Token expires in 5 minutes
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP  -- User expires in 7 days
 );
 
 CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(200) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE
