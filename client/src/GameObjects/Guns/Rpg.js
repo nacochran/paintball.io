48d03122e550091f557b199da3ea6c10367d2834
Eric Garcia <99206077+Eric-dot-png@users.noreply.github.com>
2025-03-17 10:03:19 -0500

    Reworked gun

---
+++ client/src/GameObjects/Guns/Rpg.js
@@ -0,0 +1,15 @@
+import ProjectileGun from "./ProjectileGun";
+
+export default class RPG extends ProjectileGun
+{
+    constructor(config={})
+    {
+        super({
+            name:'RPG',
+            fireRate : 2,
+            magazineSize: 1,
+            damage: 75,
+            range: 200,
+        });
+    }
+}
