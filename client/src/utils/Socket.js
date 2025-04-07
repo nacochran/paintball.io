7b2c02cb2606aaad80eabda39dc8651dd49ae1d1
Nathan Cochran <58408117+nacochran@users.noreply.github.com>
2025-04-06 21:30:46 -0500

    Testing.

--- client/src/utils/Socket.js
+++ client/src/utils/Socket.js
@@ -36,13 +36,13 @@ export default class SocketManager {
   establish_connection(arena_id, user, onStart) {
     this.arena = arena_id;
 
+    this.socket = io("https://ancient-beach-65819-22e4a65f5327.herokuapp.com/");
+    // this.socket = io("http://localhost:5000");
+
+    console.log("Socket ID: ", this.socket.id);
+
     this.socket.once('connect', () => {
       //console.log('Connected to server via WebSocket. My ID:', this.socket.id);
-
-      this.socket = io("https://ancient-beach-65819-22e4a65f5327.herokuapp.com/");
-      // this.socket = io("http://localhost:5000");
-
-      console.log("Socket ID: ", this.socket.id);
 
       // Join arena after connecting
       this.socket.emit('join-arena', {
