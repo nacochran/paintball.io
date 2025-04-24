2ac9745025e639c10f26fd392405fffe016553d8
Nathan Cochran <58408117+nacochran@users.noreply.github.com>
2025-04-24 00:04:58 -0500

    Testing.

--- client/src/GameObjects/Player.js
+++ client/src/GameObjects/Player.js
@@ -97,7 +97,7 @@ export default class Player extends PhysicsEntity {
 
     const pistolShape = new Shape({
       type: "gltf",
-      url: "client/public/assets/gltf/pistol/pistol.glb",
+      url: "/assets/gltf/pistol/pistol.glb",
       size: { width: 1, height: 1, depth: 1 },
       position: new THREE.Vector3(0, 0, 0),
       collidable: false,
